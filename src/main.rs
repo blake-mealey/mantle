01bc1d608e48e85cdc67c668d9ec9f224e55eb38
Blake Mealey <blakemealey@gmail.com>
2021-10-23 21:16:40 -0500

    get current git branch

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate clap;
 use clap::{Arg, App, SubCommand, AppSettings};
 use std::fs;
+use std::str;
 use std::collections::HashMap;
+use std::process::Command;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -32,12 +34,26 @@ fn command_publish(project_file: &str, experience_id: u64, place_id: u64) {
 }
 
 fn command_deploy(config_file: &str) {
-    println!("read {}", config_file);
+    println!("Deploying based on config file {}", config_file);
 
     let data = fs::read_to_string(config_file).expect("Unable to read file.");
     let config: Config = toml::from_str(&data).expect("Unable to parse file.");
 
-    let current_branch = "main";
+    let result = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .arg("/C")
+                .arg("git symbolic-ref --short HEAD")
+                .output()
+                .expect("Unable to check git branch.")
+    } else {
+        Command::new("sh")
+                .arg("-c")
+                .arg("git symbolic-ref --short HEAD")
+                .output()
+                .expect("Unable to check git branch.")
+    };
+
+    let current_branch = str::from_utf8(&result.stdout).unwrap().trim();
 
     let branches = config.branches.expect("No branches configuration");
 
@@ -47,18 +63,20 @@ fn command_deploy(config_file: &str) {
         return;
     }
 
-    match branch_config.unwrap().mode {
-        Some(BranchMode::Publish) => command_publish(
+    println!("Deploying with the config for branch {}", current_branch);
+
+    let mode = branch_config.unwrap().mode.as_ref();
+    match mode.unwrap_or(&BranchMode::Publish) {
+        BranchMode::Publish => command_publish(
             &config.file.unwrap(),
             branch_config.unwrap().experience_id.unwrap(),
             branch_config.unwrap().place_id.unwrap()
         ),
-        Some(BranchMode::Save) => command_save(
+        BranchMode::Save => command_save(
             &config.file.unwrap(),
             branch_config.unwrap().experience_id.unwrap(),
             branch_config.unwrap().place_id.unwrap()
-        ),
-        None => println!("No mode")
+        )
     }
 }
 

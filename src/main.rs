8c4fc9a21fc2af3ce75b7f9e9b32054bed1f2117
Blake Mealey <blakemealey@gmail.com>
2021-10-23 20:23:18 -0500

    add CLI wrapper with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate clap;
+use clap::{Arg, App, SubCommand, AppSettings};
 use std::fs;
 use std::collections::HashMap;
 use serde::Deserialize;
@@ -21,27 +23,109 @@ struct BranchConfig {
     mode: Option<BranchMode>
 }
 
-fn main() {
-    let data = fs::read_to_string("rocat.toml").expect("Unable to read file.");
+fn command_save(project_file: &str, experience_id: u64, place_id: u64) {
+    println!("save {} to {} > {}", project_file, experience_id, place_id);
+}
+
+fn command_publish(project_file: &str, experience_id: u64, place_id: u64) {
+    println!("publish {} to {} > {}", project_file, experience_id, place_id);
+}
+
+fn command_deploy(config_file: &str) {
+    println!("read {}", config_file);
+
+    let data = fs::read_to_string(config_file).expect("Unable to read file.");
     let config: Config = toml::from_str(&data).expect("Unable to parse file.");
 
-    if config.file.is_some() {
-        println!("file {}", config.file.unwrap());
+    let current_branch = "main";
+
+    let branches = config.branches.expect("No branches configuration");
+
+    let branch_config = branches.get(current_branch);
+    if branch_config.is_none() {
+        println!("No config for current branch {}", current_branch);
+        return;
     }
 
-    if config.branches.is_some() {
-        println!("branches");
+    match branch_config.unwrap().mode {
+        Some(BranchMode::Publish) => command_publish(
+            &config.file.unwrap(),
+            branch_config.unwrap().experience_id.unwrap(),
+            branch_config.unwrap().place_id.unwrap()
+        ),
+        Some(BranchMode::Save) => command_save(
+            &config.file.unwrap(),
+            branch_config.unwrap().experience_id.unwrap(),
+            branch_config.unwrap().place_id.unwrap()
+        ),
+        None => println!("No mode")
+    }
+}
 
-        for (key, value) in config.branches.unwrap() {
-            println!("{:?}", value.experience_id.unwrap());
-            println!("{:?}", value.place_id.unwrap());
-            if value.mode.is_some() {
-                match value.mode.unwrap() {
-                    BranchMode::Publish => println!("publish"),
-                    BranchMode::Save => println!("save"),
-                    _ => println!("none")
-                }
-            }
+fn main() {
+    let matches = App::new("rocat")
+        .version("0.1.0")
+        .about("Manages Roblox deployments")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand(SubCommand::with_name("save")
+            .about("Saves a project file to a Roblox place")
+            .arg(Arg::with_name("FILE")
+                .required(true)
+                .index(1)
+                .takes_value(true))
+            .arg(Arg::with_name("EXPERIENCE_ID")
+                .required(true)
+                .index(2)
+                .takes_value(true))
+            .arg(Arg::with_name("PLACE_ID")
+                .required(true)
+                .index(3)
+                .takes_value(true)))
+        .subcommand(SubCommand::with_name("publish")
+            .about("Publishes a project file to a Roblox place")
+            .arg(Arg::with_name("FILE")
+                .required(true)
+                .index(1)
+                .takes_value(true))
+            .arg(Arg::with_name("EXPERIENCE_ID")
+                .required(true)
+                .index(2)
+                .takes_value(true))
+            .arg(Arg::with_name("PLACE_ID")
+                .required(true)
+                .index(3)
+                .takes_value(true)))
+        .subcommand(SubCommand::with_name("deploy")
+            .about("Saves a project file to a Roblox place")
+            .arg(Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom deploy config file")
+                .default_value("rocat.toml")
+                .takes_value(true)))
+        .get_matches();
+
+    match matches.subcommand() {
+        ("save", Some(save_matches)) => {
+            command_save(
+                save_matches.value_of("FILE").unwrap(),
+                save_matches.value_of("EXPERIENCE_ID").unwrap().parse::<u64>().unwrap(),
+                save_matches.value_of("PLACE_ID").unwrap().parse::<u64>().unwrap()
+            );
         }
+        ("publish", Some(publish_matches)) => {
+            command_publish(
+                publish_matches.value_of("FILE").unwrap(),
+                publish_matches.value_of("EXPERIENCE_ID").unwrap().parse::<u64>().unwrap(),
+                publish_matches.value_of("PLACE_ID").unwrap().parse::<u64>().unwrap()
+            );
+        }
+        ("deploy", Some(deploy_matches)) => {
+            command_deploy(
+                deploy_matches.value_of("config").unwrap()
+            );
+        }
+        _ => {}
     }
 }

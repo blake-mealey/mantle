775896208f61c3ac9ff0ab8825dc3bf8ef7f099c
Blake Mealey <blakemealey@gmail.com>
2021-10-24 13:24:49 -0500

    improved robustness

--- src/main.rs
+++ src/main.rs
@@ -32,23 +32,25 @@ enum ProjectType {
     Binary
 }
 
-fn upload_place(project_file: &str, experience_id: u64, place_id: u64, mode: BranchMode) {
-    let api_key: &str = &env::var("ROBLOX_API_KEY").expect("No API key");
+fn upload_place(project_file: &str, experience_id: u64, place_id: u64, mode: BranchMode) -> Result<(), String> {
+    let api_key = &match env::var("ROBLOX_API_KEY") {
+        Ok(val) => val,
+        Err(_) => return Err(format!("ROBLOX_API_KEY environment variable not set"))
+    };
 
-    //let file_extension = project_file.split(".")[-1];
-    let file_extension = Path::new(project_file).extension().and_then(OsStr::to_str).expect("No file extension");
-    let project_type = (match file_extension {
-        "rbxlx" => Some(ProjectType::Xml),
-        "rbxl" => Some(ProjectType::Binary),
-        _ => None
-    }).expect(&format!("Unknown project file format {}", file_extension));
+    let project_type = match Path::new(project_file).extension().and_then(OsStr::to_str) {
+        Some("rbxmx") => ProjectType::Xml,
+        Some("rbxlx") => ProjectType::Binary,
+        Some(v) => return Err(format!("Invalid project file extension {}", v)),
+        None => return Err(format!("No project file extension"))
+    };
 
-    let content_type: &str = match project_type {
+    let content_type = match project_type {
         ProjectType::Xml => "application/xml",
         ProjectType::Binary => "application/octet-stream"
     };
 
-    let version_type: &str = match mode {
+    let version_type = match mode {
         BranchMode::Publish => "Published",
         BranchMode::Save => "Saved"
     };
@@ -69,65 +71,78 @@ fn upload_place(project_file: &str, experience_id: u64, place_id: u64, mode: Bra
         }
     };
 
-    match res {
-        Ok(response) => println!("{}", response.into_string().unwrap()),
-        Err(ureq::Error::Status(code, response)) => println!("{}", response.into_string().unwrap()),
-        Err(_) => println!("Generic error")
+    return match res {
+        Ok(response) => {
+            println!("{}", response.into_string().unwrap());
+            Ok(())
+        },
+        Err(ureq::Error::Status(_code, response)) => Err(format!("{}", response.into_string().unwrap())),
+        Err(e) => Err(format!("Generic error: {}", e))
     }
 }
 
-fn command_save(project_file: &str, experience_id: u64, place_id: u64) {
+fn command_save(project_file: &str, experience_id: u64, place_id: u64) -> Result<(), String> {
     println!("save {} to {} > {}", project_file, experience_id, place_id);
-    upload_place(project_file, experience_id, place_id, BranchMode::Save);
+    return upload_place(project_file, experience_id, place_id, BranchMode::Save);
 }
 
-fn command_publish(project_file: &str, experience_id: u64, place_id: u64) {
+fn command_publish(project_file: &str, experience_id: u64, place_id: u64) -> Result<(), String> {
     println!("publish {} to {} > {}", project_file, experience_id, place_id);
-    upload_place(project_file, experience_id, place_id, BranchMode::Publish);
+    return upload_place(project_file, experience_id, place_id, BranchMode::Publish);
 }
 
-fn command_deploy(project_file: &str, config_file: &str) {
+fn command_deploy(project_file: &str, config_file: &str) -> Result<(), String> {
     println!("Deploying based on config file {}", config_file);
 
-    let data = fs::read_to_string(config_file).expect("Unable to read file.");
-    let config: Config = toml::from_str(&data).expect("Unable to parse file.");
+    let data = match fs::read_to_string(config_file) {
+        Ok(v) => v,
+        Err(_e) => return Err(format!("Unable to read config file: {}", config_file))
+    };
 
-    let result = if cfg!(target_os = "windows") {
+    let config: Config = match toml::from_str(&data) {
+        Ok(v) => v,
+        Err(_e) => return Err(format!("Unable to parse config file {}", config_file))
+    };
+
+    let output = if cfg!(target_os = "windows") {
         Command::new("cmd")
                 .arg("/C")
                 .arg("git symbolic-ref --short HEAD")
                 .output()
-                .expect("Unable to check git branch.")
     } else {
         Command::new("sh")
                 .arg("-c")
                 .arg("git symbolic-ref --short HEAD")
                 .output()
-                .expect("Unable to check git branch.")
+    };
+
+    let result = match output {
+        Ok(v) => v,
+        Err(_e) => return Err(format!("Unable to determine current git branch. Are you in a git repository?"))
     };
 
     let current_branch = str::from_utf8(&result.stdout).unwrap().trim();
+    println!("Operating on current branch: {}", current_branch);
 
-    let branches = config.branches.expect("No branches configuration");
+    let branches = match config.branches {
+        Some(v) => v,
+        None => return Err(format!("No branch configurations found"))
+    };
 
-    let branch_config = branches.get(current_branch);
-    if branch_config.is_none() {
-        println!("No config for current branch {}", current_branch);
-        return;
-    }
+    let branch_config = match branches.get(current_branch) {
+        Some(v) => v,
+        None => return Err(format!("No branch configuration found for current branch"))
+    };
 
-    println!("Deploying with the config for branch {}", current_branch);
-    
-    let mode_ref = branch_config.unwrap().mode.as_ref();
-    let mode = match mode_ref.unwrap_or(&BranchMode::Publish) {
+    let mode = match branch_config.mode.as_ref().unwrap_or(&BranchMode::Publish) {
         &BranchMode::Publish => BranchMode::Publish,
         &BranchMode::Save => BranchMode::Save
     };
 
-    upload_place(
+    return upload_place(
         project_file,
-        branch_config.unwrap().experience_id.unwrap(),
-        branch_config.unwrap().place_id.unwrap(),
+        branch_config.experience_id.unwrap(),
+        branch_config.place_id.unwrap(),
         mode
     );
 }
@@ -180,27 +195,32 @@ fn main() {
                 .takes_value(true)))
         .get_matches();
 
-    match matches.subcommand() {
+    let result = match matches.subcommand() {
         ("save", Some(save_matches)) => {
             command_save(
                 save_matches.value_of("FILE").unwrap(),
                 save_matches.value_of("EXPERIENCE_ID").unwrap().parse::<u64>().unwrap(),
                 save_matches.value_of("PLACE_ID").unwrap().parse::<u64>().unwrap()
-            );
+            )
         }
         ("publish", Some(publish_matches)) => {
             command_publish(
                 publish_matches.value_of("FILE").unwrap(),
                 publish_matches.value_of("EXPERIENCE_ID").unwrap().parse::<u64>().unwrap(),
                 publish_matches.value_of("PLACE_ID").unwrap().parse::<u64>().unwrap()
-            );
+            )
         }
         ("deploy", Some(deploy_matches)) => {
             command_deploy(
                 deploy_matches.value_of("FILE").unwrap(),
                 deploy_matches.value_of("config").unwrap()
-            );
+            )
         }
-        _ => {}
-    }
+        _ => Err("".to_string())
+    };
+
+    match result {
+        Ok(_) => println!("success"),
+        Err(e) => println!("{}", e)
+    };
 }

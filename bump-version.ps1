8cd934051b948d39d153b429a4dbbd398522740d
Blake Mealey <blakemealey@gmail.com>
2021-10-26 11:39:22 -0500

    added bump-version script to automate releases

---
+++ bump-version.ps1
@@ -0,0 +1,18 @@
+function Get-PackageVersion() {
+  $PackageMeta = ConvertFrom-Json -InputObject $(cargo metadata --format-version 1)
+  return ($PackageMeta.packages | where { $_.name -eq "rocat" }).version
+}
+
+$PreviousVersion = Get-PackageVersion
+
+cargo install cargo-bump
+cargo bump patch
+
+$NextVersion = Get-PackageVersion
+
+$Readme = ((Get-Content -Path README.md -Raw) -replace "version = ""$PreviousVersion""","version = ""$NextVersion""").Trim()
+Set-Content -Path README.md $Readme
+
+git tag $NextVersion
+
+Write-Host "🎉 Bumped Rocat version to $NextVersion. Ready to push!"

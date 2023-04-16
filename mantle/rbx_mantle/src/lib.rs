7be5ea221e222184b73a0bb738c2e9edf1498a34
Blake Mealey <blakemealey@gmail.com>
2023-04-15 21:59:13 -0500

    remove commented out mods

--- mantle/rbx_mantle/src/lib.rs
+++ mantle/rbx_mantle/src/lib.rs
@@ -1,9 +1,7 @@
 pub mod config;
 pub mod project;
 pub mod resource_graph;
-// pub mod resource_graph_v2;
 pub mod resource_graph_v2;
-// pub mod resources;
 pub mod resources_v2;
 pub mod roblox_resource_manager;
 pub mod state;

1c47c974d7081f98074b6c448b72ad46d06b4009
Blake Mealey <blakemealey@gmail.com>
2023-07-12 13:30:45 -0500

    docs: add vercel analytics (#191)

--- docs/site/pages/_app.tsx
+++ docs/site/pages/_app.tsx
@@ -1,6 +1,12 @@
 import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import '../styles.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }

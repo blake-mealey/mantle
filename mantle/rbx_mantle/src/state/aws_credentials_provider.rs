38dbd1a0f95dee456a3e76603155a07dc0b96e8e
Juniper Hovey <me@juniper.link>
2023-12-07 10:31:29 -0600

    Fix aws credentials provider

--- mantle/rbx_mantle/src/state/aws_credentials_provider.rs
+++ mantle/rbx_mantle/src/state/aws_credentials_provider.rs
@@ -17,31 +17,32 @@ pub struct AwsCredentialsProvider {
 
 impl AwsCredentialsProvider {
     pub fn new() -> AwsCredentialsProvider {
-        let mut all_providers = AwsCredentialsProvider {
-            prefixed_environment_provider: EnvironmentProvider::with_prefix("MANTLE_AWS"),
-            environment_provider: EnvironmentProvider::default(),
-            profile_provider: ProfileProvider::new().ok(),
-            container_provider: None,
-            instance_metadata_provider: None,
-        };
-
+        let mut inherit_iam_role = false;
         if let Ok(value) = env::var("MANTLE_AWS_INHERIT_IAM_ROLE") {
             if value == "true" {
-                all_providers.container_provider = Some(ContainerProvider::new());
-                all_providers
-                    .container_provider
-                    .expect("ContainerProvider should be initialized")
-                    .set_timeout(Duration::from_secs(15));
-
-                all_providers.instance_metadata_provider = Some(InstanceMetadataProvider::new());
-                all_providers
-                    .instance_metadata_provider
-                    .expect("InstanceMetadataProvider should be initialized")
-                    .set_timeout(Duration::from_secs(15));
+                inherit_iam_role = true;
             }
         }
 
-        return all_providers;
+        AwsCredentialsProvider {
+            prefixed_environment_provider: EnvironmentProvider::with_prefix("MANTLE_AWS"),
+            environment_provider: EnvironmentProvider::default(),
+            profile_provider: ProfileProvider::new().ok(),
+            container_provider: if inherit_iam_role {
+                let mut provider = ContainerProvider::new();
+                provider.set_timeout(Duration::from_secs(15));
+                Some(provider)
+            } else {
+                None
+            },
+            instance_metadata_provider: if inherit_iam_role {
+                let mut provider = InstanceMetadataProvider::new();
+                provider.set_timeout(Duration::from_secs(15));
+                Some(provider)
+            } else {
+                None
+            },
+        }
     }
 }
 
@@ -59,11 +60,15 @@ async fn chain_provider_credentials(
             return Ok(creds);
         }
     }
-    if let Ok(creds) = provider.container_provider.credentials().await {
-        return Ok(creds);
+    if let Some(ref container_provider) = provider.container_provider {
+        if let Ok(creds) = container_provider.credentials().await {
+            return Ok(creds);
+        }
     }
-    if let Ok(creds) = provider.instance_metadata_provider.credentials().await {
-        return Ok(creds);
+    if let Some(ref instance_metadata_provider) = provider.instance_metadata_provider {
+        if let Ok(creds) = instance_metadata_provider.credentials().await {
+            return Ok(creds);
+        }
     }
     Err(CredentialsError::new(
         "Couldn't find AWS credentials in environment, credentials file, or instance/container IAM role.",
